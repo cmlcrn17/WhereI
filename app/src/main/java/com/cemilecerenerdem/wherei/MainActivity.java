1c19aa2994172ea60dfa8adad5c7643db6f91c83
ceren <cerdem@geobilgi.com>
2019-03-03 23:25:39 +0300

    MainAct update

--- app/src/main/java/com/cemilecerenerdem/wherei/MainActivity.java
+++ app/src/main/java/com/cemilecerenerdem/wherei/MainActivity.java
@@ -5,16 +5,23 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.ConnectivityManager;
 import android.os.Build;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -32,8 +39,6 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     public static LocationListener locationListener;
     LatLng userLocation;
 
-    //TODO: 1- Uygulamaya icon bulunacak.
-
     @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +46,31 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_main);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
 
+        Boolean isInternet = isInternet();
+
+        if (isInternet == true) {
+            mapFragment.getMapAsync(this);
+        } else {
+
+            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+            alertBuilder.setView(R.layout.dialog_warning);
+            alertBuilder.setCancelable(false);
+            alertBuilder.setPositiveButton(R.string.dialog_PositiveButton_OpenInternet, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    onActivityResult(1, 1, null);
+                }
+            });
+            alertBuilder.setNegativeButton(R.string.dialog_NegativeButton_CloseApp, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    onActivityResult(0, 0, null);
+                }
+            });
+
+            alertBuilder.create().show();
+        }
     }
 
     @Override
@@ -97,28 +125,170 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    public void WriteMeLocation(View v) {
+    public void ShareMeLocation(View v) {
 
-        Dialog dialog = new Dialog(this);
-        dialog.setContentView(R.layout.dialog_msg);
-        dialog.setTitle(getString(R.string.dialog_msgTitle));
+        if (isInternet() == true) {
+            Dialog dialog = new Dialog(this);
+            dialog.setContentView(R.layout.dialog_msg);
+            dialog.setTitle(getString(R.string.dialog_msgTitle));
 
-        TextView info = (TextView) dialog.findViewById(R.id.txt_view);
+            TextView info = (TextView) dialog.findViewById(R.id.txt_view);
 
-        if (userLocation.latitude != 0 && userLocation.longitude != 0) {
-            info.setText("Latidude: " + userLocation.latitude + " - Longitude: " + userLocation.longitude);
+            if (userLocation.latitude != 0 && userLocation.longitude != 0) {
 
-            Button whatsApp = (Button) dialog.findViewById(R.id.btn_whts);
-            Button twitter = (Button) dialog.findViewById(R.id.btn_twt);
-            Button person = (Button) dialog.findViewById(R.id.btn_person);
+                //TODO: 2- Dialog Alert içerisinde google dan alınan konum bilgisi mahalle sokak kapı no olarak yazdırılacak.
+                final String location = "Latidude: " + userLocation.latitude + " - Longitude: " + userLocation.longitude;
+                info.setText(location);
 
+                Button whatsApp = (Button) dialog.findViewById(R.id.btn_whts);
+                Button twitter = (Button) dialog.findViewById(R.id.btn_twt);
+                Button contact = (Button) dialog.findViewById(R.id.btn_contact);
+
+                //WhatsApp
+                whatsApp.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        //TODO 19- WhatsApp olan android telefonda test edilecek.
+                        if (isInternet() == true) {
+                            //TODO: 6- WhatsApp kişi listesi açılacak.
+                        } else {
+                            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+                            alertBuilder.setView(R.layout.dialog_warning);
+                            alertBuilder.setCancelable(false);
+                            alertBuilder.setPositiveButton(R.string.dialog_PositiveButton_OpenInternet, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    onActivityResult(1, 1, null);
+                                }
+                            });
+                            alertBuilder.setNegativeButton(R.string.dialog_NegativeButton_CloseApp, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    onActivityResult(0, 0, null);
+                                }
+                            });
+
+                            alertBuilder.create().show();
+                        }
+                    }
+                });
+
+                //Twitter
+                twitter.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (isInternet() == true) {
+                            //TODO: 15- Twitterda paylaş yapılacak.
+                            //TODO: 18- Twittera giriş yapılarak test edilecek.
+                        } else {
+                            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+                            alertBuilder.setView(R.layout.dialog_warning);
+                            alertBuilder.setCancelable(false);
+                            alertBuilder.setPositiveButton(R.string.dialog_PositiveButton_OpenInternet, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    onActivityResult(1, 1, null);
+                                }
+                            });
+                            alertBuilder.setNegativeButton(R.string.dialog_NegativeButton_CloseApp, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    onActivityResult(0, 0, null);
+                                }
+                            });
+
+                            alertBuilder.create().show();
+                        }
+                    }
+                });
+
+                //Contact Guide
+                contact.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //TODO: 16- Mesaj gönderilerek test edilecek.
+                        //TODO: 17- Kişi listesi gelecek. Seçilen kişiye direk gönderim yapılacak.
+                        if (isInternet() == true) {
+                            Intent sendIntent = new Intent();
+                            sendIntent.setAction(Intent.ACTION_SEND);
+                            sendIntent.putExtra(Intent.EXTRA_TEXT, "My location is " + location);
+                            sendIntent.setType("text/plain");
+                            startActivity(sendIntent);
+                            //TODO: 19- Mesaj gönderimi başarılı ise mesaj göster.
+                        } else {
+                            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+                            alertBuilder.setView(R.layout.dialog_warning);
+                            alertBuilder.setCancelable(false);
+                            alertBuilder.setPositiveButton(R.string.dialog_PositiveButton_OpenInternet, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    onActivityResult(1, 1, null);
+                                }
+                            });
+                            alertBuilder.setNegativeButton(R.string.dialog_NegativeButton_CloseApp, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    onActivityResult(0, 0, null);
+                                }
+                            });
+
+                            alertBuilder.create().show();
+                        }
+                    }
+                });
+
+            } else {
+                info.setText(getString(R.string.text_YourLocationNotFound));
+            }
+            dialog.show();
         } else {
-            info.setText("Your location not found.");
+            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+            alertBuilder.setView(R.layout.dialog_warning);
+            alertBuilder.setCancelable(false);
+            alertBuilder.setPositiveButton(R.string.dialog_PositiveButton_OpenInternet, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    onActivityResult(1, 1, null);
+                }
+            });
+            alertBuilder.setNegativeButton(R.string.dialog_NegativeButton_CloseApp, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    onActivityResult(0, 0, null);
+                }
+            });
+
+            alertBuilder.create().show();
         }
+    }
 
-        dialog.show();
+    public boolean isInternet() {
 
-        //TODO: 2- Dialog Alert içerisinde google dan alınan konum bilgisi mahalle sokak kapı no olarak yazdırılacak.
-        //TODO: 3- Dialog Alert içerisine paylaş butonları işlemleri yazılacak..
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager.getActiveNetworkInfo() != null) {
+            connectivityManager.getActiveNetworkInfo().isConnected();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Comment
+        // requestCode, resultCode 1: Open Internet
+
+        if (requestCode == 1) { //Open Internet
+            if (resultCode == 1) {
+                //TODO: 14- İnterneti açtır
+            } else {
+                finish();
+            }
+        } else {
+            finish();
+        }
     }
 }
